d6a153c9f4e840a0d59165d314ac3f875465513f
rorfan8782 <cormacdev@gmail.com>
2017-05-29 21:26:28 +0100

    Protytyping V2

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -18,19 +18,28 @@ function MainCtrl($http, $state, $filter, Upload) {
     showWeeks: false
   };
   ctrl.altDateFormats = ['M!/d!/yyyy']; // Date validation format
-  ctrl.itemCategories = ['Missing', 'Cosmetic', 'Broken', 'Extra']; // Conciliation Type
 
+  ctrl.receiptDateOpened = false;
   ctrl.step; // Edit step
+  ctrl.hasCosmetic;
+  ctrl.hasBroken;
+  ctrl.hasMissing;
+  ctrl.hasExtra;
   ctrl.stepTitles = [
     'Who is checking in this PO?',
-    'Enter Purchase Order Information'
+    'Enter Purchase Order Information',
+    'Were there Cosmetic Items?',
+    'Were there Broken Items?',
+    'Were there any missing items?',
+    'Were there any extra items?',
+    'Summary of PO Change'
   ];
 
   ctrl.checkers = [
-    'Mike Kobalski',
-    'Cort Ulas',
-    'Tommy Caveny',
-    'Jeremy Vestal'
+    { name: 'Mike Kobalski', email: 'tom@aamedicalstore.com', phone: '708-479-0062'},
+    { name: 'Cort Ulas', email: 'mike@aamedicalstore.com', phone: '708-479-0062' },
+    { name: 'Tommy Caveny', email: 'jeremy@aamedicalstore.com', phone: '708-479-0062' },
+    { name: 'Jeremy Vestal', email: 'cort@aamedicalstore.com', phone: '708-479-0062' }
   ];
 
   // AWS Configuration
@@ -42,6 +51,12 @@ function MainCtrl($http, $state, $filter, Upload) {
     ctrl.error = null;
   }
 
+  // Open date picker for Receipt Date
+  ctrl.openReceiptDate = function() {
+    ctrl.receiptDateOpened = true;
+  }
+
+  // Go to next step
   ctrl.goNext = function(form) {
     if (!form.$valid) {
       return;
@@ -50,12 +65,157 @@ function MainCtrl($http, $state, $filter, Upload) {
     ctrl.step ++;
   }
 
+  // Go to previous step
   ctrl.goPrev = function() {
     ctrl.step --;
   }
 
+  ctrl.changeHasCosmetic = function() {
+    if (ctrl.hasCosmetic == 'yes') {
+      var item = new Object();
+      item.category = 'Cosmetic';
+      item.moreCosmetic = 'no';
+      ctrl.po.cosmetics.push(item);
+    } else {
+      ctrl.po.cosmetics = [];
+    }
+  }
+
+  ctrl.addCosmetic = function(index, item) {
+    if (item.moreCosmetic == 'yes') {
+      var newItem = new Object();
+      newItem.category = 'Cosmetic';
+      ctrl.po.cosmetics.push(newItem);
+    } else {
+      ctrl.po.cosmetics.splice(index + 1, 1);
+    }
+  }
+
+  ctrl.changeHasBroken = function() {
+    if (ctrl.hasBroken == 'yes') {
+      var item = new Object();
+      item.category = 'Broken';
+      item.moreBroken = 'no';
+      ctrl.po.brokens.push(item);
+    } else {
+      ctrl.po.brokens = [];
+    }
+  }
+
+  ctrl.addBroken = function(index, item) {
+    if (item.moreBroken == 'yes') {
+      var newItem = new Object();
+      newItem.category = 'Broken';
+      ctrl.po.brokens.push(newItem);
+    } else {
+      ctrl.po.brokens.splice(index + 1, 1);
+    }
+  }
+
+  ctrl.changeHasMissing = function() {
+    if (ctrl.hasMissing == 'yes') {
+      var item = new Object();
+      item.category = 'Missing';
+      item.moreMissing = 'no';
+      ctrl.po.missings.push(item);
+    } else {
+      ctrl.po.missings.splice(i, 1);
+    }
+  }
+
+  ctrl.addMissing = function(index, item) {
+    if (item.moreMissing == 'yes') {
+      var newItem = new Object();
+      newItem.category = 'Missing';
+      ctrl.po.missings.push(newItem);
+    } else {
+      ctrl.po.missings.splice(index + 1, 1);
+    }
+  }
+
+  ctrl.changeHasExtra = function() {
+    if (ctrl.hasExtra == 'yes') {
+      var item = new Object();
+      item.category = 'Extra'
+      item.moreExtra = 'no';
+      ctrl.po.extras.push(item);
+    } else {
+      ctrl.po.extras.splice(i, 1);
+    }
+  }
+
+  ctrl.addExtra = function(index, item) {
+    if (item.moreExtra == 'yes') {
+      var newItem = new Object();
+      newItem.category = 'Extra';
+      ctrl.po.extras.push(newItem);
+    } else {
+      ctrl.po.extras.splice(index + 1, 1);
+    }
+  }
+
+  // Get items list by category
+  ctrl.getItems = function(category) {
+    if (category) {
+      var list = [];
+      for (var i = 0; i < ctrl.po.items.length; i++) {
+        if (ctrl.po.items[i].category == category) {
+          list.push(ctrl.po.items[i]);
+        }
+      }
+
+      return list;
+    } else {
+      return ctrl.po.items;
+    }
+  }
+
+  // Submit total data
+  ctrl.submit = function(form) {
+    if (!form.$valid) {
+      return;
+    }
+
+    // Upload files
+    var bucket = new AWS.S3({ params: { Bucket: 'poconciliation', maxRetries: 1 }, httpOptions: { timeout: 360000 } });
+    var options = {
+      partSize: 10 * 1024 * 1024,
+      queueSize: 1,
+      ACL: 'bucket-owner-full-control'
+    };
+
+    var items = [];
+    angular.extend(items, ctrl.cosmetics, ctrl.brokens, ctrl.missings, ctrl.extras);
+    var filesCount = 0, uploadedCount = 0;
+    for (var i = 0; i < items.length; i++) {
+      var file = items[i].file;
+      if (file) {
+        filesCount ++;
+        items[i].fileName = items[i].productNumber + '-' + file.name;
+        var params = {
+          Bucket: 'poconciliation',
+          Key: items[i].fileName,
+          ContentType: file.type,
+          Body: file
+        };
+
+        bucket.upload(params, options, function(err, data) {
+          uploadedCount ++;
+
+          if (uploadedCount == filesCount) { // All files were uploaded
+            postParams();
+          }
+        });
+      }
+    }
+
+    if (uploadedCount == 0) {
+      postParams(items);
+    }
+  }
+
   // Post form data to API endpoint
-  function postParams() {
+  function postParams(items) {
     $http({
       method: 'POST',
       url: '/api/send',
@@ -67,19 +227,21 @@ function MainCtrl($http, $state, $filter, Upload) {
         return str.join("&");
       },
       data: {
+        checkerName: ctrl.po.checker.name,
+        checkerEmail: ctrl.po.checker.email,
+        checkerPhone: ctrl.po.checker.phone,
+        emailTo: ctrl.po.emailTo,
         poNumber: ctrl.po.poNumber,
+        poAmount: ctrl.po.poAmount,
+        receiptDate: $filter('date')(ctrl.po.receiptDate, 'MM/dd/yyyy'),
+        accountName: ctrl.po.accountName,
+        contactName: ctrl.po.contactName,
         orgPurchasePrice: $filter('currency')(ctrl.po.orgPurchasePrice, '$', 2),
         receivedValue: $filter('currency')(ctrl.po.receivedValue, '$', 2),
         difference: $filter('currency')(ctrl.po.difference, '$', 2),
-        receiptDate: $filter('date')(ctrl.po.receiptDate, 'MM/dd/yyyy'),
-        name: ctrl.po.contactName,
-        email: ctrl.po.contactEmail,
-        phoneNumber: ctrl.po.contactPhone,
-        items: JSON.stringify(ctrl.po.items),
-        emailTo: ctrl.po.emailTo
+        items: JSON.stringify(items),
       }
     }).then(function(response) {
-      $state.go('done', {po: ctrl.po, success: true});
     }, function(error) {
       ctrl.error = 'Failed to send email. Please try again.';
     });
@@ -87,6 +249,14 @@ function MainCtrl($http, $state, $filter, Upload) {
 
   function init() {
     ctrl.step = 0;
+    ctrl.hasCosmetic = 'no';
+    ctrl.hasBroken = 'no';
+    ctrl.hasMissing = 'no';
+    ctrl.hasExtra = 'no';
+    ctrl.po.cosmetics = [];
+    ctrl.po.brokens = [];
+    ctrl.po.missings = [];
+    ctrl.po.extras = [];
   }
 
   init();
